a046e4de141d9f6466de4007fc16451d70cacdbc
Travis CI <ci@travis-ci.org>
2017-10-31 13:18:54 +0000

    Updates

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,12 +1,11 @@
 npm run build
 
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO}
-git config --global user.name "QC-L"
-git config --global user.email "liqichang_4869@163.com"
-git remote set-url origin "${SSH_REPO}"
+git config --global user.name "Travis CI"
+git config --global user.email "ci@travis-ci.org"
+git remote set-url origin git@github.com:docschina/babeljs.io.git
 
-openssl aes-256-cbc -K $encrypted_9d42543f6ef0_key -iv $encrypted_9d42543f6ef0_iv -in shell/deploy_key.enc -out shell/deploy_key -d
+
+openssl aes-256-cbc -K $encrypted_6f04629492b4_key -iv $encrypted_6f04629492b4_iv -in shell/deploy_key.enc -out shell/deploy_key -d
 chmod 600 shell/deploy_key
 eval `ssh-agent -s`
 ssh-add shell/deploy_key
